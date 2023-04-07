47a70d62ed13817de8c01b0aa07dfadc077d7c6b
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-07 10:58:53 +0600

    Change home on New.js b change locations on App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import About from "./Pages/About";
+import Contacts from "./Pages/Contacts";
+import Delivery from "./Pages/Delivery";
+import New from "./Pages/New";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/contact" element={<h1>Contact</h1>} />
-          <Route path="/delivery" element={<h1>Delivery</h1>} />
+          <Route path="/" element={<New />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/delivery" element={<Delivery />} />
         </Routes>
       </Layout>
     </div>
--- src/Pages/Home.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="Home">
-      <h1>Home</h1>
-      <p>This is the home page</p>
-    </div>
-  );
-}
