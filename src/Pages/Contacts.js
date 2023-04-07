d133094b7cb9c96f38923addff86e79a4f94520d
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-07 10:57:09 +0600

    Aaa new file at the name pages and changed their names in App Js

---
+++ src/Pages/Contacts.js
@@ -0,0 +1,8 @@
+export default function Contacts() {
+  return (
+    <div className="Contacts">
+      <h1>Delivery</h1>
+      <p>This is the contacts page</p>
+    </div>
+  )
+}
---
+++ src/Pages/Delivery.js
@@ -0,0 +1,8 @@
+export default function Delivery() {
+  return (
+    <div className="Devilery">
+      <h1>Delivery</h1>
+      <p>This is the devilery page</p>
+    </div>
+  )
+}
