72840aa2828a9b4d3a1e3fa5f7cb32796091b418
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-12 14:45:38 +0600

    remove the useEffect and replace it with AppContent and remove some imports from the forward function

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,36 +1,12 @@
 import { NavLink } from "react-router-dom";
 import "./CategoryList.css";
-import { categories } from "../../firebase";
-import { getDocs } from "firebase/firestore/lite";
-import { useEffect, useState } from "react";
+import { useContext} from "react";
+import { AppContext } from "../../App";
 
 export default function CategoryList() {
-  const [categoryList, setCategoryList] = useState([]);
+  const { categories} = useContext(AppContext);
 
-  useEffect(() => {
-    getDocs(categories).then((snapshot) => {
-    const newCategoryList = [];
-    snapshot.docs.forEach((doc) => {
-      const category = doc.data();
-      category.id = doc.id;
-
-      newCategoryList.push(category);
-      })
-      setCategoryList(newCategoryList);
-    });
-  }, []);
-
-  // const categories = [
-  //   {id: 1, name: "Dj", slug: "dj"},
-  //   {id: 2, name: "Drones", slug: "drones"},
-  //   {id: 3, name: "Cameras", slug: "cameras"},
-  //   {id: 4, name: "Backpacks and cases", slug: "backpacks-and-cases"},
-  //   {id: 5, name: "Electronic steadicams", slug: "electronic-steadicams"},
-  //   {id: 6, name: "Underwater equipment", slug: "underwater-equipment"},
-  //   {id: 7, name: "Photo/Video", slug: "photo-video"},
-  // ]
-
-  const output = categoryList.map((category) => (
+  const output = categories.map((category) => (
     <li key={category.id}>
       <NavLink to={"/categories/" + category.slug}>{category.name}</NavLink>
     </li>
