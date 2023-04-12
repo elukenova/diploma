94528b97070ab70edffc05e304d97a6224aaeeb6
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-12 15:19:35 +0600

    remove array from Category and change it with AppContext and see if there are any errors

--- src/Pages/Category.js
+++ src/Pages/Category.js
@@ -1,18 +1,12 @@
 import { useMatch } from "react-router-dom";
 import NotFound from "./NotFound";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 export default function Category() {
   const { params } = useMatch("/categories/:slug");
 
-  const categories = [
-    { id: 1, name: "Dj", slug: "dj" },
-    { id: 2, name: "Drones", slug: "drones" },
-    { id: 3, name: "Cameras", slug: "cameras" },
-    { id: 4, name: "Backpacks and cases", slug: "backpacks-and-cases" },
-    { id: 5, name: "Electronic steadicams", slug: "electronic-steadicams" },
-    { id: 6, name: "Underwater equipment", slug: "underwater-equipment" },
-    { id: 7, name: "Photo/Video", slug: "photo-video" },
-  ];
+  const {categories} = useContext(AppContext);
 
   const category = categories.find((category) => category.slug === params.slug);
 
