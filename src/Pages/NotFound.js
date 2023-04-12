74027020d339d9b31ba0c7496c1dd21947e33ca7
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-12 14:21:28 +0600

    add new page NotFound this page will be used when the user is misused slug

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -2,7 +2,7 @@ export default function  NotFound (){
   return (
     <div className="NotFound">
       <h1>404</h1>
-      <h2>Everyone left to eat ashlyam-fu</h2>
+      <h2>Everyone left to eat ashlyan-fu</h2>
     </div>
   )
 }
