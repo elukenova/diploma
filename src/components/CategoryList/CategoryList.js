d922227aec49f57bff7468f80deb708f8cebe32c
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-17 13:14:09 +0600

    remove some unnecessary rows and add some elements

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -5,12 +5,12 @@ import { AppContext } from "../../App";
 
 export default function CategoryList() {
   const { categories} = useContext(AppContext);
-
   const output = categories.map((category) => (
     <li key={category.id}>
+      <img src={category.icon} alt={category.name} />
       <NavLink to={"/categories/" + category.slug}>{category.name}</NavLink>
     </li>
-  ));
+  )); 
   return (
     <div className="CategoryList">
       <h1>Categories</h1>
