4a190aeb792a91f7b4cb0713860aff6d0bce1b8d
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-17 14:26:46 +0600

    add a click to a loaf and change it like a cart

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,8 +1,20 @@
+import { useContext } from "react";
 import "./AddToCart.css";
+import { AppContext } from "../../App";
 export default function AddToCart({product}){
+  const {cart, setCart}=useContext(AppContext)
+
+  function onAddToCartClick(){
+    setCart({
+      ...cart, //положить текущее содержание корзинки
+      [product.id]: 1 //добавить текущий товар
+    });
+
+  }
+
   return(
     <div className="AddToCart">
-      <button>Add to cart</button>
+      <button onClick={onAddToCartClick}>Add to cart</button>
     </div>
-  )
+  );
 }
