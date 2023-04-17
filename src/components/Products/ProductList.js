41076bad94396415ca71cc8e0468734acd379e4a
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-17 13:17:48 +0600

    removed some elements and changed the property

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -2,21 +2,17 @@ import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
 import "./ProductList.css";
-export default function ProductList(){
-  const {products} = useContext(AppContext);
+export default function ProductList({ category }) {
+  const { products } = useContext(AppContext);
 
-  const output = products.map(product => (
-  <div>
-    <img src={product.picture} alt={product.name} />
-    <NavLink to={'/product/' + product.slug}>
-      {product.name}
-    </NavLink>
-    <span>{product.price} $</span>
-  </div>
-  ));
-  return (
-    <div className="ProductList">
-      {output}
-    </div>
-  )
+  const output = products
+    .filter((product) => product.category === category.id)
+    .map((product) => (
+      <div key={product.id} className="Product">
+        <img src={product.picture} alt={product.name} />
+        <NavLink to={"/products/" + product.slug}>{product.name}</NavLink>
+        <span>{product.price} $</span>
+      </div>
+    ));
+  return <div className="ProductList">{output}</div>;
 }
