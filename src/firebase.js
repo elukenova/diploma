fbdd2236689a6512c7dbe3ec32d44897a79a7fa0
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-19 15:33:28 +0600

    add some elements from firebase so that the user can log into his account

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from 'firebase/firestore/lite';
+import {getAuth} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +20,7 @@ const app = initializeApp(firebaseConfig);
 
 // Create a reference to the database service
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 
 export const categoryCollection = collection(db, "categories");
