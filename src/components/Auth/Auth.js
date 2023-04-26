46886874dba11160081c315a594d369fa54347ec
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-26 14:03:18 +0600

    change signin on login and signout on logout and change some elements

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,15 +1,24 @@
 import { useContext } from "react";
-import { signIn, signOff } from "../../firebase";
+import { logIn, logOut } from "../../firebase";
 import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
 
 export default function Auth() {
   const { user } = useContext(AppContext);
 
   return (
     <div className="Auth">
-      {!user ? <button onClick={signIn}>Sign in</button> : null}
-      {user ? <span>{user.displayName}</span> : null}
-      {user ? <button onClick={signOff}>Sign out</button> : null}
+      {user ? (
+        <span>
+          Hello <Link to="/orders">{user.displayName}</Link>!
+          <button onClick={logOut}>Sign out</button>
+        </span>
+      ) : (
+        <span>
+          Hello guest!
+          <button onClick={logIn}>Sign in</button>
+        </span>
+      )}
     </div>
   );
 }
