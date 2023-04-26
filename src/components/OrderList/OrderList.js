39280226b8dfcd21bcc56b65d3fa961186af2943
Aijanyl Elukenova <ajowka05@gmail.com>
2023-04-26 13:52:51 +0600

    on this page the buyer can see his order

---
+++ src/components/OrderList/OrderList.js
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import "./OrderList.css";
+import { AppContext } from "../../App";
+
+export default function OrderList(){
+  const { orders, products } = useContext(AppContext);
+
+  if(!orders.length || !products.length){
+    return "No orders found.";
+  }
+  // Выводим все заказы.
+  const output = orders.map(order => {
+    // Вывести содержимое корзины для этого заказа
+    const cartOutput = Object.keys(order.cart).map(productId => {
+      const product = products.find(product => product.id === productId);
+
+      if (!product){
+        return "Product not found";
+      }
+
+      return (
+        <div>
+          <img src={product.picture} alt={product.name} />
+          {product.name}: {order.cart[productId]} X {product.price} $ = {order.cart [productId] *product.price} som
+        </div>
+      );
+    })
+
+    return (
+      <div className="Order">
+        <div>Name: {order.name}</div>
+        <div>Phone: {order.phone}</div>
+        <div>Address: {order.address}</div>
+        <div>Cart: {cartOutput}</div>
+      </div>
+    );
+  })
+
+  return (
+    <div className="OrderList">
+      {output}
+    </div>
+  );
+}
