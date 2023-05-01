9134e411146836c6ccbc0cf220cbe2a893f4d397
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-01 14:30:33 +0600

    added adding and deleting a category and imported it into  CategoryList.js

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -2,19 +2,22 @@ import { NavLink } from "react-router-dom";
 import "./CategoryList.css";
 import { useContext } from "react";
 import { AppContext } from "../../App";
+import AddCategory from "../AddCategory/AddCategory";
+import DeleteCategory from "../DeleteCategory/DeleteCategory";
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
   const output = categories.map((category) => (
     <li key={category.id}>
-      
       <NavLink to={"/categories/" + category.slug}>{category.name}</NavLink>
+      <DeleteCategory category={category} />
     </li>
   ));
   return (
     <div className="CategoryList">
       <h1>Categories</h1>
       <ul>{output}</ul>
+      <AddCategory />
     </div>
   );
 }
