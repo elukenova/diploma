7ad9ce15fd5e77a8126215a052e725b7ce707734
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-03 14:10:13 +0600

    change some elements and add plus

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -21,7 +21,7 @@ export default function OrderList(){
       return (
         <div>
           <img src={product.picture} alt={product.name} />
-          {product.name}: {order.cart[productId]} X {product.price} $ = {order.cart [productId] *product.price} som
+          {product.name}: {order.cart[productId]} X {product.price} $ = {order.cart [productId] * product.price} + som;
         </div>
       );
     })
