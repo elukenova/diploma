2529cbc900dccb394a41887ca0d1d796641255fa
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-11 15:34:49 +0600

    can't find white sigin and just change it

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -13,7 +13,7 @@ export default function Auth() {
       {user ? (
         <span>
           <Link to="/orders">{user.displayName}</Link>!
-          <img className="Test" src="https://cdn4.iconfinder.com/data/icons/contact-us-19/48/92-512.png" alt="signIn"   onClick={logOut}/>
+          <img className="Test" src="https://www.clipartmax.com/png/middle/336-3366677_login-login-icon-white-png.png" alt="signIn"   onClick={logOut}/>
         </span>
       ) : (
         <span>
