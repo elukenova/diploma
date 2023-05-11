10f81c24a8086febb09eabeebfad02bd5bfd9493
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-11 14:37:00 +0600

    add texts and photos to the about us page and just write information

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,17 @@
+import "../components/About.css";
 export default function About() {
   return (
     <div className="About">
-      <h1>Delivery</h1>
+      <h1>SkyLand</h1>
       <p>This is the about page</p>
+      <div className="card-item">
+      <img src="https://nationaltoday.com/wp-content/uploads/2022/05/International-Drone-Day-.jpg" alt="drone"/>
+      <span>Sky Land Management provides high quality, innovative aerial and traditional ground based land management solutions.  Since 2014, we have used the unique capabilities of the latest precision drone technology to provide safer, targeted, efficient and cost effective solutions for clients, in conjunction with traditional ground based services.</span>
+      <img src="https://www.jouav.com/wp-content/uploads/2022/10/heavy-lift-drone-1024x683.jpg" alt="drone" />
+      <span>Sky Land Management continues to lead the way in the use of Remotely Piloted Aircraft Systems (RPAS), commonly referred to as drones, for aerial application such as weed spraying and seeding.  With our recently upgraded RPAS, we are at the forefront of technology, employing the latest autonomous, precision application drones for spraying and spreading.  This latest technology provides the added benefits of swarming, using up to 5 drones working together simultaneously.  In addition, we use an integrated survey drone providing precision mapping to enhance planning and deployment of the drones.   
+      </span>
+      
+      </div>
     </div>
   )
 }
