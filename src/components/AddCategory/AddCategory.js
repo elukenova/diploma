b981417592d8e6a3abf2ac8e84a232e2787bede5
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-15 14:34:04 +0600

    here I added elements so that adding a category would not show an error

--- src/components/AddCategory/AddCategory.js
+++ src/components/AddCategory/AddCategory.js
@@ -1,45 +1,60 @@
-import React, { useContext, useState } from "react";
-import "./AddCategory.css";
 import { addDoc } from "firebase/firestore";
+import { useContext, useState } from "react";
 import { categoryCollection } from "../../firebase";
 import { AppContext } from "../../App";
 
-export default function AddCategory() {
+const AddCategory = () => {
   const { user } = useContext(AppContext);
   const [category, setCategory] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  if (!user || !user.isAdmin){
+  if (!user || !user.isAdmin) {
     return null;
   }
+
   function onChangeCategory(event) {
     setCategory(event.target.value);
   }
 
   function onAddCategory() {
     const name = category.trim();
-    if (name.length <= 5) {
+
+    if (name.length < 5) {
       alert(
-        "Plase provide a category name with minimum length of 5 characters"
+        "Please provide a category name with minimum length of 5 characters."
       );
+
       return;
     }
+
+    setIsSubmitting(true);
+
     addDoc(categoryCollection, {
       name: name,
-      slug: category.trim().replaceAll(" ", "-").toLocaleLowerCase(),
-    }).then(() => {
-      setCategory("");
-    });
+      slug: name.replaceAll(" ", "-").toLocaleLowerCase(),
+    })
+      .then(() => {
+        setCategory("");
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
   }
+
   return (
     <div className="AddCategory">
       <input
         size="15"
         type="text"
         value={category}
-        placeholder="Category Name"
+        placeholder="Category name"
         onChange={onChangeCategory}
       />
-      <button onClick={onAddCategory}>+</button>
+      <button onClick={onAddCategory} disabled={isSubmitting}>
+        +
+      </button>
     </div>
   );
-}
+};
+
+export default AddCategory;
