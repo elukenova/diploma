7404aed78eb2950914005111601a39637ff69ba7
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-16 14:10:41 +0600

    changed login icon and imported it to Auth

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { logIn, logOut } from "../../firebase";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
+import signin from "../../assets/login.svg";
 import "./Auth.css";
 
 
@@ -13,11 +14,11 @@ export default function Auth() {
       {user ? (
         <span>
           <Link to="/orders">{user.displayName}</Link>!
-          <img className="Test" src="https://img.favpng.com/1/19/13/disconnect-icon-logout-sign-icon-interface-icon-u9vq3TKg_t.jpg" alt="signIn"   onClick={logOut}/>
+          <img className="Test" src="https://www.clipartmax.com/png/middle/336-3366677_login-login-icon-white-png.png" alt="signIn"   onClick={logOut}/>
         </span>
       ) : (
         <span>
-          <img className="Test" src="https://cdn4.iconfinder.com/data/icons/contact-us-19/48/92-512.png" alt="signOut"  onClick={logIn}/>
+          <img className="Test" src={signin} alt="signOut"  onClick={logIn}/>
         </span>
       )}
     </div>
