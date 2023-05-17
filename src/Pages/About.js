300dfbabce1fb8c598ad33031f88776f218cb818
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-17 14:22:35 +0600

    changed the text and styled some elements

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,13 +4,14 @@ export default function About() {
     <div className="about">
       <h1>DJI</h1>
       <div className="card-item">
-      <img src="https://nationaltoday.com/wp-content/uploads/2022/05/International-Drone-Day-.jpg" alt="drone"/>
-      <span>Sky Land Management provides high quality, innovative aerial and traditional ground based land management solutions.  Since 2014, we have used the unique capabilities of the latest precision drone technology to provide safer, targeted, efficient and cost effective solutions for clients, in conjunction with traditional ground based services.</span>
-      <img src="https://www.jouav.com/wp-content/uploads/2022/10/heavy-lift-drone-1024x683.jpg" alt="drone" />
-      <span>Sky Land Management continues to lead the way in the use of Remotely Piloted Aircraft Systems (RPAS), commonly referred to as drones, for aerial application such as weed spraying and seeding.  With our recently upgraded RPAS, we are at the forefront of technology, employing the latest autonomous, precision application drones for spraying and spreading.  This latest technology provides the added benefits of swarming, using up to 5 drones working together simultaneously.  In addition, we use an integrated survey drone providing precision mapping to enhance planning and deployment of the drones.   
-      </span>
+      <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/1374A/production/_124309697_djigettyimages-1353421153.jpg" alt="drone"/>
+        <p>Creativity is at the heart of every dream. Every idea, every groundbreaking leap that changes our world starts with the vision of talented creators. At DJI, we give these creators the tools they need to bring their ideas to life.</p>
+        <p>Our platforms empower them to capture images that were once out of reach. Our flying and camera stabilization systems redefine camera placement and motion. Amazing photos and video, treasured personal memories, and high-end professional imagery are captured every day, in every corner of the world using DJI products.</p>
+        <p>We do this through an unparalleled commitment to R&D, a culture of constant innovation and curiosity, and a focus on transforming complex technology into easy-to-use devices. Building on the ethos of “form follows function,” our products combine advanced technology with dynamic designs.</p>
+        <p>Headquartered in Shenzhen, widely considered China’s Silicon Valley, DJI benefits from direct access to the suppliers, raw materials, and young, creative talent pool necessary for sustained success. Drawing on these resources, we have grown from a single small office in 2006 to a global workforce. Our offices can now be found in the United States, Germany, the Netherlands, Japan, South Korea, Beijing, Shanghai, and Hong Kong. As a privately owned and operated company, DJI focuses on our own vision, supporting creative, commercial, and nonprofit applications of our technology. Today, DJI products are redefining industries. Professionals in filmmaking, agriculture, conservation, search and rescue, energy infrastructure, and more trust DJI to bring new perspectives to their work and help them accomplish feats safer, faster, and with greater efficiency than ever before.</p>
       
       </div>
+  
     </div>
   )
 }
