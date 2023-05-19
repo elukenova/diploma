f48f7570d2df7f2f776e38a40a1c3badac5d1633
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-19 14:15:13 +0600

    opened for NotFound css and styled this page and imported it into js

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,8 +1,8 @@
 export default function  NotFound (){
   return (
     <div className="NotFound">
-      <h1>404</h1>
       <h2>Everyone left to eat ashlyan-fu</h2>
+      <img className="NotFound-gif" src="https://media0.giphy.com/media/UoeaPqYrimha6rdTFV/giphy.gif" alt="gif"></img>
     </div>
   )
 }
