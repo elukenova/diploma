0fad7eaf06241eb113b1a2691333d1f1b1c8e1b1
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-19 14:28:00 +0600

    import to NavToggle and Drawer and set some function

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,27 @@
 import Logo from "../Logo/Logo"
-import React from "react";
+import React, { useState } from "react";
 import Nav from "../Nav/Nav";
 import "./Layout.css";
 import CategoryList from "../CategoryList/CategoryList";
 import CartLink from "../CartLink/CartLink";
 import Auth from "../Auth/Auth";
 import Footer from "../Footer/Footer";
+import NavToggle from "../NavToggle/NavToggle";
+import Drawer from "../Drawer/Drawer";
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  
+    function toggleDrawer() {
+      setDrawerOpen(!drawerOpen);
+    }
   return (
     <div className="Layout">
       <header>
         <Logo />
         <Nav />
+        <NavToggle callback={toggleDrawer} />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
         <CartLink />
         <Auth />
       </header>
