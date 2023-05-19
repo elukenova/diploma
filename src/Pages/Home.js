843f509fc9a09514a81b979efdbb62535be1e73b
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-19 14:16:22 +0600

    I wanted to make a video query on the main page, but it didn’t work out and I only imported a video from YouTube

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <div className="Home">
       <p>We will show you new products and promotions. Or just let you know about drones.</p>
+      <div className="drone-video">
+        <div className="video"><iframe width="560" height="315" src="https://www.youtube.com/embed/_mgf2hjX7jg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+        <img className="avata" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzuYgucHSpah0sgOA3edN6X_SNOKgq78nSFw&usqp=CAU" alt="avata"/>
+        <div className="video2"><iframe width="560" height="315" src="https://www.youtube.com/embed/C6cgcpfKt3g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div></div>
+        <div className="video3"><iframe width="560" height="315" src="https://www.youtube.com/embed/jEh0XyGhIxo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
     </div>
   );
 }
