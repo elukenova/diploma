9cfe9860b558dc9e89f0301b46d1492fbe0e20a3
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-19 14:23:58 +0600

    opened a file here to add a burger to the navigation

---
+++ src/components/Drawer/Drawer.js
@@ -0,0 +1,15 @@
+import Nav from "../Nav/Nav";
+import "./Drawer.css"
+
+function Drawer({ open, toggle }) {
+  const drawerClassNames = `Drawer ${open ? "open" : ""}`;
+  return (
+    <div className={drawerClassNames}>
+      <div onClick={toggle} className="backdrop"></div>
+      <div className="content">
+        <Nav />
+      </div>
+    </div>
+  );
+}
+export default Drawer;
