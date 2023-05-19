54657f71708790f963986e104ae1b29ba0864f2c
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-19 14:13:24 +0600

    added a gif and styled this page

--- src/Pages/ThankYou.js
+++ src/Pages/ThankYou.js
@@ -1,8 +1,10 @@
+import "../components/ThankYou.css";
 export default function ThankYou() {
   return (
     <div className="ThankYou">
       <h1>Thank you!</h1>
       <p>Your order has been received. Our specialists will contact with you shortly to confirm it.</p>
+      <img className="ThankYou-gif"  src="https://i.pinimg.com/originals/3b/06/4b/3b064b3b1b06b15202567dc8337e972f.gif" alt="gif"/>
     </div>
   );
 }
