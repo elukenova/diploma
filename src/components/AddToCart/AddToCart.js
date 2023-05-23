750b36ca7abf72ea76bb06e1d34714349b4f196c
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-23 14:05:52 +0600

    I wrapped the counter with a span to style it separately

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,22 +1,24 @@
 import { useContext } from "react";
 import "./AddToCart.css";
 import { AppContext } from "../../App";
-export default function AddToCart({product}){
-  const {cart, setCart}=useContext(AppContext)
+export default function AddToCart({ product }) {
+  const { cart, setCart } = useContext(AppContext);
 
-  function onAddToCartClick(){
-    const qty =cart[product.id] ? cart[product.id] +1 : 1;
+  function onAddToCartClick() {
+    const qty = cart[product.id] ? cart[product.id] + 1 : 1;
     setCart({
       ...cart, //положить текущее содержание корзинки
-      [product.id]: qty,  //добавить текущий товар
+      [product.id]: qty, //добавить текущий товар
     });
-
   }
 
-  return(
+  return (
     <div className="AddToCart">
-      {cart[product.id] ? cart[product.id] : 0}<br className="center"/>
-      <button className="btn-cart" onClick={onAddToCartClick}>Add to cart</button>
+      <span className="total">{cart[product.id] ? cart[product.id] : 0}</span>
+      <br className="center" />
+      <button className="btn-cart" onClick={onAddToCartClick}>
+        Add to cart
+      </button>
     </div>
   );
 }
