45b962f76267a701d031a7a314e11d9e9d127983
Aijanyl Elukenova <ajowka05@gmail.com>
2023-05-25 16:58:06 +0600

    added a function. This function sends the user to the main menu

--- src/Pages/ThankYou.js
+++ src/Pages/ThankYou.js
@@ -1,5 +1,8 @@
 import "../components/Pages.css/ThankYou.css";
 export default function ThankYou() {
+  setTimeout(() => {
+    window.location.href = "/";
+  }, 2000);
   return (
     <div className="ThankYou">
       <h1>Thank you!</h1>
